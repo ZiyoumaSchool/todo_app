1bf6fd868c7d1b337875db62bb2c153d1a59ca39
RoosveltK <roosvelt.kenne@gmail.com>
2023-07-12 03:24:38 +0100

    add todo

--- src/Components/AddTodo/index.jsx
+++ src/Components/AddTodo/index.jsx
@@ -9,13 +9,22 @@ const AddTodo = ({ handleSubmit }) => {
       setInputValue("");
     }
   };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  }
+
   return (
     <div className="row p-2">
       <div className="col-lg-10 col-md-10 col-xs-12">
         <Input
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={(e) => 
+            setInputValue(e.target.value)}
           placeholder="Entrer le nom de la tache"
+          onKeyPress={(e) => handleKeyPress(e)}
         />
       </div>
       <div className="col-lg-2 col-md-2 col-xs-12">
